788f08f1d06b9417ff1523cb8f79e518bee3b227
unknown <maurosanchez@gmail.com>
2025-11-12 22:25:43 -0300

    Descripción cambios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} Mau — Desarrollado con ❤️ y React.</p>
+      <p>© {new Date().getFullYear()} Mauro Sanchez Camps — Desarrollado con React.</p>
       <div className="socials">
-        <a href="https://github.com/tuusuario" target="_blank" rel="noreferrer">GitHub</a>
-        <a href="https://linkedin.com/in/tuusuario" target="_blank" rel="noreferrer">LinkedIn</a>
+        <a href="https://github.com/MauroSanchezCamps31" target="_blank" rel="noreferrer">GitHub</a>
+        <a href="https://www.linkedin.com/in/mauro-sanchez-camps-916879275/" target="_blank" rel="noreferrer">LinkedIn</a>
       </div>
     </footer>
   );
